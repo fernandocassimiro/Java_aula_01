b1cafcc9de6474e8dd13b82514c40cbfbeed3106
Fernando Cassimiro de Macedo <fernandocassimiro@uol.com.br>
2018-07-26 10:16:59 -0300

    Java_aula_01

---
+++ src/meprimeiroprojeto2/Meprimeiroprojeto3.java
@@ -0,0 +1,15 @@
+
+package meprimeiroprojeto2;
+
+
+public class Meprimeiroprojeto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ola mundo");
+               
+        }
+    
+}
